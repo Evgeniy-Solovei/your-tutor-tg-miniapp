0cf2e620510eab9254650e98f178c5cf4e9855b4
solovey.e.v. <e.v.solovey@inbox.ru>
2026-08-22 17:57:17 +0300

    init commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput --clear
 python manage.py setup_project || true
 
 echo "🚀 Запуск Web сервера (Gunicorn на порту 8005)..."
-exec gunicorn --workers 3 --bind 0.0.0.0:8005 tutor_bot.wsgi:application
+exec gunicorn --workers 3 --bind 0.0.0.0:8005 --access-logfile - --error-logfile - tutor_bot.wsgi:application
